12c832ef4d7df23531a57c0965ea03d6aa6612d7
Bhuvana11 <52947359+Bhuvana11@users.noreply.github.com>
2019-08-09 22:31:35 -0700

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,9 @@
+x = input()
+if(x== 'a' or x== 'e' or x == 'i' or x == 'o' or x == 'u' or x== 'A'
+       or x== 'E' or x == 'I' or x== 'O' or x== 'U'):
+    print("Vowel")
+elif("A>=x" and "Z<=x") or ("a>=x" and "z<=x"):
+    print("Consonant")
+else:
+	print("invalid")
+   	
